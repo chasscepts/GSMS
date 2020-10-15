124d51172b079a7970eda8efb56027289f645681
Raphael Olujayogbe <engrraph123@gmail.com>
2020-10-15 18:43:47 +0100

    Home Fragment design

--- app/src/main/java/com/chass/gsms/MainActivity.java
+++ app/src/main/java/com/chass/gsms/MainActivity.java
@@ -1,17 +1,15 @@
 package com.chass.gsms;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
       }
     });
   }
+
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
