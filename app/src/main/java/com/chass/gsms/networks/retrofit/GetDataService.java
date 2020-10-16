2dcb958d422ea5903225c57ee076f2523be1d7c0
chasscepts <65827072+chasscepts@users.noreply.github.com>
2020-10-16 10:31:28 +0100

    Setup (#5)
    * Added addClass method in GetDataService
    
    * Deleted Parent and Teacher User SubClasses
    
    * Updated School to have a pointer to the admin.
    Removed Roles property from User and deleted Roles enum.
    
    * Created the Student Model
    
    * Created the Class Model
    
    * Corrected api path in addClass (GetDataService)
    
    * Created StudentViewModel class and added selectStudent() to IMain and getClass to GetDataService interfaces

--- app/src/main/java/com/chass/gsms/networks/retrofit/GetDataService.java
+++ app/src/main/java/com/chass/gsms/networks/retrofit/GetDataService.java
@@ -3,9 +3,11 @@ package com.chass.gsms.networks.retrofit;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.Path;
 
 public interface GetDataService {
 
@@ -36,7 +38,7 @@ public interface GetDataService {
   );
 
   @FormUrlEncoded
-  @POST("user")
+  @POST("school/class")
   Call<String> addClass(
       @Field("school_id") String schoolId,
       @Field("class_name") String className,
@@ -45,4 +47,7 @@ public interface GetDataService {
       @Field("teacher_email") String teacherEmail,
       @Field("teacher_phone_number") String teacherPhoneNumber
   );
+
+  @GET("school/class/{name}")
+  Call<String> getClass(@Path("name") String className);
 }
