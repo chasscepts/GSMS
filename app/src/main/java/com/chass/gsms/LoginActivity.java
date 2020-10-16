32bd06bf740473c7371fd25ce472d6df3edebe16
Raphael Olujayogbe <engrraph123@gmail.com>
2020-10-16 04:56:22 +0100

    Design home fragment (#2)
    * Login page design
    
    * Home Fragment design
    
    * Login design changed
    
    Co-authored-by: chasscepts <65827072+chasscepts@users.noreply.github.com>

--- app/src/main/java/com/chass/gsms/LoginActivity.java
+++ app/src/main/java/com/chass/gsms/LoginActivity.java
@@ -1,14 +1,10 @@
 package com.chass.gsms;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
