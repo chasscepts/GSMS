805880a919adc3495d777bf44166311405cb65b6
Obetta <francischassobetta@gmail.com>
2020-10-18 06:22:15 +0100

    Created Model Classes Attendance and StudentAttendanceStatus

---
+++ app/src/main/java/com/chass/gsms/models/Attendance.java
@@ -0,0 +1,14 @@
+package com.chass.gsms.models;
+
+import java.util.Date;
+
+public class Attendance {
+  private int schoolId;
+  private String className;
+  private Date date;
+  private StudentAttendanceStatus[] attendanceStatuses;
+
+  public Attendance(){
+
+  }
+}
