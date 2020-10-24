011f5cf9fba8e70d41772257ee3e5447362b79e5
Obetta <francischassobetta@gmail.com>
2020-10-24 19:44:45 +0100

    Added attendance_status_view.xml

--- app/src/main/java/com/chass/gsms/viewmodels/AttendanceStatusViewModel.java
+++ app/src/main/java/com/chass/gsms/viewmodels/AttendanceStatusViewModel.java
@@ -1,6 +1,12 @@
 package com.chass.gsms.viewmodels;
 
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
 import androidx.databinding.ObservableField;
+
+import com.chass.gsms.R;
 
 public class AttendanceStatusViewModel {
   public enum statuses { PRESENT, ABSENT, NONE }
@@ -19,7 +25,13 @@ public class AttendanceStatusViewModel {
     status.set(statuses.NONE);
   }
 
-  public static void setStatus(){
-
+  @BindingAdapter("attendanceStatus")
+  public static void setStatus(ImageView img, statuses status){
+    if(status == statuses.NONE){
+      img.setVisibility(View.GONE);
+      return;
+    }
+    img.setVisibility(View.VISIBLE);
+    img.setImageResource(status == statuses.PRESENT? R.drawable.ic_baseline_check_box_24 : R.drawable.ic_baseline_cancel_presentation_24);
   }
 }
