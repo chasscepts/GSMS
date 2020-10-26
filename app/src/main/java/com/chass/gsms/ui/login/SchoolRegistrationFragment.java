ef6ec32bb52a52767c381480bf43469a6a49ebbd
Obetta <francischassobetta@gmail.com>
2020-10-26 09:38:16 +0100

    Put finishing touches to SchoolRegistrationFragment

--- app/src/main/java/com/chass/gsms/ui/login/SchoolRegistrationFragment.java
+++ app/src/main/java/com/chass/gsms/ui/login/SchoolRegistrationFragment.java
@@ -1,5 +1,9 @@
 package com.chass.gsms.ui.login;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,12 +11,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.chass.gsms.R;
 import com.chass.gsms.databinding.FragmentSchoolRegistrationBinding;
 
+import java.io.File;
+import java.net.URI;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class SchoolRegistrationFragment extends Fragment {
+  private static final int CHOOSE_FILE_RESULT_CODE = 5;
+  private static final int READ_FILE_PERMISSION_CODE = 7;
+
   FragmentSchoolRegistrationBinding B;
   private SchoolRegistrationViewModel viewModel;
 
@@ -21,8 +37,8 @@ public class SchoolRegistrationFragment extends Fragment {
     // Inflate the layout for this fragment
     B = FragmentSchoolRegistrationBinding.inflate(inflater, container, false);
 
-    //We scope ViewModel to the Activity to retain user input after navigating to another fragment
-    viewModel = new ViewModelProvider(requireActivity()).get(SchoolRegistrationViewModel.class);
+    //We scope all ViewModels to the Fragment and share data with SharedDataStore
+    viewModel = new ViewModelProvider(getViewModelStore(), ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().getApplication())).get(SchoolRegistrationViewModel.class);
     B.setViewModel(viewModel);
     return B.getRoot();
   }
@@ -30,6 +46,53 @@ public class SchoolRegistrationFragment extends Fragment {
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
-    //B.buttonSecond.setOnClickListener(view1 -> NavHostFragment.findNavController(SchoolRegistrationFragment.this).navigate(R.id.action_Second2Fragment_to_First2Fragment));
+    B.btnLoginRegister.setOnClickListener(view1 -> NavHostFragment.findNavController(SchoolRegistrationFragment.this).navigate(R.id.LoginFragment));
+    B.edtTxtSchoolProfilePicture.setOnClickListener(view1 -> {
+      openFileDialog();
+    });
+    B.btnRegister.setOnClickListener(view1 -> {
+      registerOrRequestFilePermissionIfNotGranted();
+    });
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                         @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    if(requestCode == READ_FILE_PERMISSION_CODE){
+      if (permissions[0].equals(Manifest.permission.READ_EXTERNAL_STORAGE)
+          && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        viewModel.register();
+      }
+    }
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if(requestCode == CHOOSE_FILE_RESULT_CODE){
+      if (resultCode == -1) {
+        Uri uri = data.getData();
+        if(uri != null){
+          viewModel.getFormViewModel().setSchoolPicture(new File(URI.create(uri.getPath())));
+        }
+      }
+    }
+  }
+
+  private void registerOrRequestFilePermissionIfNotGranted(){
+    if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED) {
+      requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, READ_FILE_PERMISSION_CODE);
+    }
+    else {
+      viewModel.register();
+    }
+  }
+
+  private void openFileDialog() {
+    Intent chooseFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+    chooseFileIntent.setType("*/*");
+    chooseFileIntent = Intent.createChooser(chooseFileIntent, "Choose Lecture File");
+    startActivityForResult(chooseFileIntent, CHOOSE_FILE_RESULT_CODE);
   }
 }
